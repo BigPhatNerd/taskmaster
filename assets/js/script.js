ec5392707fae8dd60505bf8a56a4e5548378ed11
Wilson <wilsonhorrell@git.training>
2020-07-05 11:24:37 -0500

    Deploying

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,7 @@ $("#modalDueDate").datepicker({
     minDate: 1
 });
 
+
 var createTask = function(taskText, taskDate, taskList) {
     // create elements that make up a task item
     var taskLi = $("<li>").addClass("list-group-item");
@@ -68,15 +69,20 @@ $(".card .list-group").sortable({
     // helper: "clone",
     activate: function(event) {
         console.log("activate", this);
+        $(this).addClass("dropover");
+        $(".bottom-trash").addClass("bottom-trash-drag");
     },
     deactivate: function(event) {
         console.log("deactivate", this);
+        $(this).removeClass("dropover");
+        $(".bottom-trash").removeClass("bottom-trash-drag");
     },
     over: function(event) {
         console.log("over", event.target);
+        $(event.target).addClass("dropover-active");
     },
     out: function(event) {
-        console.log("out", event.target);
+        $(event.target).removeClass("dropover-active");
     },
     update: function(event) {
         var tempArr = [];
@@ -108,12 +114,15 @@ $("#trash").droppable({
     accept: ".card .list-group-item",
     tolerance: "touch",
     drop: function(event, ui) {
+        $(".bottom-trash").removeClass("bottom-trash-active");
         ui.draggable.remove();
     },
     over: function(event, ui) {
         console.log("over");
+        $(".bottom-trash").addClass("bottom-trash-active");
     },
     out: function(event, ui) {
+        $(".bottom-trash").removeClass("bottom-trash-active");
         console.log("out");
     }
 });
@@ -215,7 +224,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
     // get form values
     var taskText = $("#modalTaskDescription").val();
     var taskDate = $("#modalDueDate").val();
@@ -247,6 +256,13 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
+
+setInterval(function() {
+    $(".card .list-group-item").each(function(el) {
+        auditTask(el);
+
+    });
+}, 5000);
 
 
 
